eae0bda639ba51a230fe1fbeaade946ee5145ef7
raiponz <spooki.leserpent@orange.fr>
2017-06-29 10:32:30 +0200

    Ajout de la class Schtroumpf.java

---
+++ src/main/java/lefonddeletang/expectoPatternum/schtroumpf/Schtroumpf.java
@@ -0,0 +1,17 @@
+package lefonddeletang.expectoPatternum.schtroumpf;
+
+/**
+ * Created by hugo on 29/06/2017.
+ */
+public class Schtroumpf {
+    private String nom;
+    private Marteau outil;
+
+    public Schtroumpf(String nom) {
+        this.nom = nom;
+    }
+
+    public void setOutil(Marteau outil) {
+        this.outil = outil;
+    }
+}
